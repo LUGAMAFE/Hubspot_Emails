3d6365f9402b5ff559321005d38d281647db4616
Luis ]avier Martinez Fernandez <luisjavier004@hotmail.com>
2022-10-25 14:22:10 -0500

    updatedcredentials

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
+const hubspot = require("@hubspot/api-client");
 
+app.get("/", async (req, res) => {
+  const hubspotClient = new hubspot.Client({
+    accessToken: "pat-na1-f4cb6a76-e92c-4200-854d-197cef6aaf98",
+  });
 
+  const properties = {
+    company: "Biglytics2",
+    email: "passwordtest@mail.net",
+    firstname: "Bryan",
+    lastname: "Cooper",
+    phone: "(877) 929-0687",
+    website: "biglytics.net",
+    password:"jejejf493209445"
+  };
+  const SimplePublicObjectInput = { properties };
 
-app.get('/', async(req, res) => {
-    const hubspot = require('@hubspot/api-client');
-
-const hubspotClient = new hubspot.Client({"accessToken":"pat-na1-2744e970-8a88-4306-aaba-d43b8033d66f"});
-
-const properties = {
-  "company": "Biglytics2",
-  "email": "bcooper2@biglytics.net",
-  "firstname": "Bryan",
-  "lastname": "Cooper",
-  "phone": "(877) 929-0687",
-  "website": "biglytics.net",
-  "rol":"supercustomer"
-};
-const SimplePublicObjectInput = { properties };
-
-try {
-  const apiResponse = await hubspotClient.crm.contacts.basicApi.create(SimplePublicObjectInput);
-  console.log(JSON.stringify(apiResponse.body, null, 2));
-} catch (e) {
-  e.message === 'HTTP request failed'
-    ? console.error(JSON.stringify(e.response, null, 2))
-    : console.error(e)
-}
-    res.send('Hello World!')
-    })
+  try {
+    const apiResponse = await hubspotClient.crm.contacts.basicApi.create(
+      SimplePublicObjectInput
+    );
+    console.log(JSON.stringify(apiResponse.body, null, 2));
+  } catch (e) {
+    e.message === "HTTP request failed"
+      ? console.error(JSON.stringify(e.response, null, 2))
+      : console.error(e);
+  }
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
